bbaf62e28ea1e9899c62b2f9d139a1f128e3a165
Sasha Kryvko <sashakrivko75@gmail.com>
2024-07-22 23:46:27 +0200

    Added unit of work

---
+++ src/mousse/mousse.Application/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace mousse.Application.Abstractions;
+
+public interface IUnitOfWork
+{
+    Task SaveChangesAsync(CancellationToken token = default);
+}
---
+++ src/mousse/mousse.Persistanse/UnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using mousse.Domain.Core.Repositories;
+
+namespace mousse.Persistence;
+
+public class UnitOfWork(MousseContext context) : IUnitOfWork
+{
+    public async Task<int> SaveChangesAsync(CancellationToken token = default)
+    {
+        return await context.SaveChangesAsync(token);
+    }
+}
