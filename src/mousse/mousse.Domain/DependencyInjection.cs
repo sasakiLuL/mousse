9a5a3ffe5a057f71829dede32e28d2c576d5d87a
Sasha Kryvko <sashakrivko75@gmail.com>
2024-07-22 23:58:24 +0200

    Updated domain dependency injections

---
+++ src/mousse/mousse.Domain/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using mousse.Domain.Accesses;
+using mousse.Domain.Followers;
+
+namespace mousse.Domain;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDomain(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IAccessService, AccessService>();
+
+        services.AddScoped<IFollowerService, FollowerService>();
+
+        return services;
+    }
+}
