7e46fe8aa355d2db1a60abe43204e4f659625fb0
Sasha Kryvko <sashakrivko75@gmail.com>
2024-09-30 03:50:06 +0200

    Added api extensions

---
+++ src/mousse.Api/Extensions/MigrationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mousse.Api.Extensions;
+
+public static class MigrationExtensions
+{
+    public static void ApplyMigrations<TContext>(this IApplicationBuilder app) where TContext : DbContext
+    {
+        using IServiceScope serviceScope = app.ApplicationServices.CreateScope();
+
+        using TContext context =
+            serviceScope.ServiceProvider.GetRequiredService<TContext>();
+
+        context.Database.Migrate();
+    }
+}
---
+++ src/mousse.Api/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace mousse.Api.Extensions;
+
+public static class SwaggerExtensions
+{
+    public static IServiceCollection AddSwaggerWithAuth(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+
+        services.AddSwaggerGen(o =>
+        {
+            o.CustomSchemaIds(id => id.FullName!.Replace('+', '-'));
+
+            o.AddSecurityDefinition("Keycloak", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.OAuth2,
+                Flows = new OpenApiOAuthFlows
+                {
+                    Implicit = new OpenApiOAuthFlow
+                    {
+                        AuthorizationUrl = new Uri("http://localhost:8080/realms/mousse/protocol/openid-connect/auth"),
+                        Scopes = new Dictionary<string, string>
+                {
+                    { "openid", "openid" },
+                    { "profile", "profile" }
+                }
+                    }
+                }
+            });
+
+            o.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Id = "Keycloak",
+                            Type = ReferenceType.SecurityScheme
+                        },
+                        In = ParameterLocation.Header,
+                        Name = "Bearer",
+                        Scheme = "Bearer"
+                    },
+                    []
+                }
+            });
+        });
+
+        return services;
+    }
+}
